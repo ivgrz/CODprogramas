f3211aa65ff7afd2dfcd5b1cebc5ecfee6bd97d5
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-10 13:12:26 +0200

    ejercicio 1 boletin 2_2

--- boletin3/src/main/java/org/example/Main.java
+++ boletin3/src/main/java/org/example/Main.java
@@ -1,9 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//Bolentin22ej1
+        Double pagado, tarifa, porcent;
+
+        Scanner entradaTec = new Scanner(System.in);
+        System.out.println("introduce el precio pagado");
+        pagado = entradaTec.nextDouble();
+
+        System.out.println("introduce la tarifa");
+        tarifa = entradaTec.nextDouble();
+
+        porcent = 100 - ((pagado/tarifa) * 100);
+        System.out.println("El porcentaje realizado fue de: "+ porcent + "%");
+//Boletin 22 ej2
+
+
+
+
+
 
 
     }
