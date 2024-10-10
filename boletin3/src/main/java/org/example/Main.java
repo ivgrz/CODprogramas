34165edc9d7d82942f8decd8341b626ab777f788
Ivan <igutierrezcontreras@danielcastelao.org>
2024-10-10 13:27:06 +0200

    ejercicio 2 boletin 2_2

--- boletin3/src/main/java/org/example/Main.java
+++ boletin3/src/main/java/org/example/Main.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //Bolentin22ej1
-        Double pagado, tarifa, porcent;
+        /*
+        double pagado, tarifa, porcent;
 
         Scanner entradaTec = new Scanner(System.in);
         System.out.println("introduce el precio pagado");
@@ -16,9 +17,26 @@ public class Main {
         System.out.println("introduce la tarifa");
         tarifa = entradaTec.nextDouble();
 
-        porcent = 100 - ((pagado/tarifa) * 100);
+        porcent = (();
+
+
         System.out.println("El porcentaje realizado fue de: "+ porcent + "%");
+
+         */
 //Boletin 22 ej2
+
+double  c, ef, ek;
+    Scanner eTec = new Scanner(System.in);
+
+    System.out.println("ingresa la temperatura en ºC:");
+        c = eTec.nextDouble();
+
+        ef = ((c * 5)/9) + 32;
+        System.out.println("La temperatura en farenheit es:" + ef + " grados farenheit");
+
+        ek = (c + 273);
+        System.out.println("La temperatura en kelvin es:" + ek + " grados kelvin");
+
 
 
 
